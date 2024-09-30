e3eaa79fb65aac8692c9ef7891315fe2fc112e2c
Kim daehyeon <113977176+Dh3356@users.noreply.github.com>
2024-09-30 22:31:43 +0900

    [level 2] Title: 기능개발, Time: 0.51 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(int[] progresses, int[] speeds) {
+        List<Integer> answer = new LinkedList<>();
+        
+        for(int i = 0; i < progresses.length; i++){
+            int value = progresses[i];
+            int days = (int) Math.ceil((100 - value) / (float)speeds[i]);
+    
+            for(int j = i; j<progresses.length; j++){
+                progresses[j] += speeds[j] * days;
+            }
+            
+            int sum = 0;
+            while(i < progresses.length){
+                if(progresses[i] < 100){
+                    break;
+                }
+                sum++;
+                i++;
+            }
+            i--;
+            answer.add(sum);
+        }
+        return answer;
+    }
+}
