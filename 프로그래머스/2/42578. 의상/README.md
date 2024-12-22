b9f73f25768c52f829c4a24f955ba1e3eb6d976e
Kim daehyeon <113977176+Dh3356@users.noreply.github.com>
2024-12-22 22:29:56 +0900

    [level 2] Title: 의상, Time: 0.21 ms, Memory: 94.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=java) 
 
 ### 성능 요약
 
-메모리: 78.9 MB, 시간: 0.12 ms
+메모리: 94.4 MB, 시간: 0.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 30일 21:14:32
+2024년 12월 22일 22:29:53
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,22 +1,30 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
-        HashMap<String, Integer> clothesMap = new HashMap<>();
-        
-        for(String[] c : clothes){
-            clothesMap.put(c[1], clothesMap.getOrDefault(c[1], 0) + 1);
+
+        int answer = 0;
+
+        HashMap<String, List<String>> clothesMap = new HashMap<>();
+
+        for (String[] clothe : clothes) {
+            List<String> value = clothesMap.getOrDefault(clothe[1], new LinkedList<>());
+            value.add(clothe[0]);
+            clothesMap.put(clothe[1], value);
+            answer++;
         }
-        
-        if(clothesMap.keySet().size() == 1){
-            return clothes.length;
+
+        if (clothesMap.size() == 1) {
+            return answer;
         }
-        
-        for(String i : clothesMap.keySet()){
-            answer *= clothesMap.get(i) + 1;
+
+        int answer2 = 1;
+        for (String key : clothesMap.keySet()) {
+            answer2 *= clothesMap.get(key).size() + 1;
         }
-        
-        return answer - 1;
+
+        return answer2 - 1;
     }
 }
